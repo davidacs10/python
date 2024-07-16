91c1746673bd659f6988993bd97eb0c4307eb028
davidacs10 <davidacs10.08@gmail.com>
2024-07-16 14:37:12 -0400

    01_helloworld.py

---
+++ 30-Days-Of-Python/01_helloworld.py
@@ -0,0 +1,50 @@
+# Exercises level 1
+
+print(3 + 4)  # addition
+print(3 - 4)  # subtraction
+print(3 * 4)  # multiplication
+print(3 / 4)  # division
+print(3 % 4)  # modulus
+print(3**4)  # exponential
+print(3 // 4)  # floor division operator
+
+print("David")
+print("Campos Specht")
+print("Venezuela")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["David", "Python", "Venezuela"]))
+print(type("David"))
+print(type("Campos Specht"))
+print(type("Venezuela"))
+
+# Exercises level 2
+
+# 1 Write an example for different Python data types such as Number
+# (Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+
+print(9)
+print(type(9))
+print(10.40)
+print(type(10.40))
+print(4j)
+print(type(4j))
+print("Hello World")
+print(type("Hello World"))
+print(True)
+print(type(True))
+print([1, 2, 3, 4, 5])
+print(type([1, 2, 3, 4, 5]))
+print((1, 2, 3, 4, 5))
+print(type((1, 2, 3, 4, 5)))
+print({1, 2, 3, 4, 5, 5})
+print(type({1, 2, 3, 4, 5, 5}))
+print({"name": "David", "lastname": "Campos", "age": 26})
+print(type({"name": "David", "lastname": "Campos", "age": 26}))
+
+# 2 Find an Euclidian distance between (2, 3) and (10, 8)
+print(((2 - 10) ** 2 + (3 - 8) ** 2) ** 0.5)
